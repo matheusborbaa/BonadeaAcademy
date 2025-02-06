5e14ab331abae8831db83a2f34410b4c02596feb
matborba2 <110839515+matheusborbaa@users.noreply.github.com>
2025-02-05 21:17:12 -0300

    Merge branch 'main' of https://github.com/matheusborbaa/BonadeaAcademy

--- templates/template-inicio.php
+++ templates/template-inicio.php
@@ -1,5 +1,4 @@
-
-    <?php
+<?php
 
 /**
  * Template Name: Início
@@ -7,196 +6,89 @@
 
 get_header();
 ?>
-    <section class="hero" id="home">
-        
-    </section>
 
-    <section class="services" id="beleza">
-        <h2>Nossos serviços</h2>
-        <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis ex nihil dolore non
-            doloremque harum.</p>
+<section class="section-one">
+    <div class="slide-home" style="background-color: #000; background-image: url('<?php echo _URLTHEME; ?>/assets/img/videoinicio.png')">
+        <div class="content">
+        <div class="btnbusca"><i class="fa fa-search" style="margin: 0 1px 1px 0;align-items: center; justify-content:center;"></i><input type="text" /></div>
+        <div class="btnLogin"><i class="fa fa-user"></i></div>
+        <img src="<?php echo _URLTHEME; ?>/assets/img/playbutton.png" alt="" class="playvideo">
+            <div class="content-icons">
+                <ul class="socials">
+                    <li><a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                    <li><a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href=""><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+            <div class="content-text">
+                <h1>Seja uma Profissional Bonadea</h1>
 
-        <div class="services-carousel">
-            <div class="service-card">
-                <div class="service-image"></div>
-                <h3>Beauty Care</h3>
-                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
             </div>
-            <div class="service-card">
-                <div class="service-image"></div>
-                <h3>Body Care</h3>
-                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
-            </div>
-            <div class="service-card">
-                <div class="service-image"></div>
-                <h3>Face Care</h3>
-                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
-            </div>
-            <div class="service-card">
-                <div class="service-image"></div>
-                <h3>Hair Care</h3>
-                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
-            </div>
-            <div class="service-card">
-                <div class="service-image"></div>
-                <h3>Hair Care</h3>
-                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
-            </div>
-            <div class="service-card">
-                <div class="service-image"></div>
-                <h3>Hair Care</h3>
-                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
-            </div>
-            <div class="service-card">
-                <div class="service-image"></div>
-                <h3>Hair Care</h3>
-                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
-            </div>
+            <a href='#'> <button class="btn" href="" src="#">Saiba mais </button></a>
+
         </div>
-        <div class="scroll-indicators">
-            <span class="indicator active"></span>
-            <span class="indicator"></span>
-            <span class="indicator"></span>
-        </div>
-    </section>
 
-    <section class="testimonials">
-        <div class="testimonials-carousel">
-            <div class="testimonials-grid">
-                <!-- Primeiro par de cards -->
-                <div class="testimonial-card">
-                    <div class="testimonial-image">
-                        <img src="assets/mulher3.jpg" alt="Teresa Sobral" />
-                    </div>
-                    <div class="testimonial-content">
-                        <p class="testimonial-quote">
-                            Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.
-                        </p>
-                        <h3 class="testimonial-author">Teresa Sobral</h3>
-                        <p class="testimonial-role">/ Advogada</p>
-                    </div>
-                </div>
-    
-                <div class="testimonial-card">
-                    <div class="testimonial-image">
-                        <img src="assets/mulher2.jpg" alt="Amanda Batista" />
-                    </div>
-                    <div class="testimonial-content">
-                        <p class="testimonial-quote">
-                            Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.
-                        </p>
-                        <h3 class="testimonial-author">Amanda Batista</h3>
-                        <p class="testimonial-role">/ Engenheira</p>
-                    </div>
-                </div>
-                
-    
-                <!-- Segundo par de cards -->
-                <div class="testimonial-card">
-                    <div class="testimonial-image">
-                        <img src="assets/mulher1.jpg" alt="Carla Lopez" />
-                    </div>
-                    <div class="testimonial-content">
-                        <p class="testimonial-quote">
-                            Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.
-                        </p>
-                        <h3 class="testimonial-author">Carla Lopez</h3>
-                        <p class="testimonial-role">/ Professora</p>
-                    </div>
-                </div>
-    
-                <div class="testimonial-card">
-                    <div class="testimonial-image">
-                        <img src="assets/image 10.png" alt="Jennifer" />
-                    </div>
-                    <div class="testimonial-content">
-                        <p class="testimonial-quote">
-                            Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.
-                        </p>
-                        <h3 class="testimonial-author">Jennifer</h3>
-                        <p class="testimonial-role">/ Lojista</p>
-                    </div>
-                </div>
+    </div>
+</section>
+<section class="section-two">
+    <div class="content-two">
+        <div class="imagem" style="background-image: url('<?php echo _URLTHEME; ?>/assets/img/sobrecurso.png'); ">
+        </div>
+        <div class="content-two-text">
+            <h1>SOBRE O CURSO</h1>
+            <h3>
+           <p class=""> O curso de Formação Profissional “O Poder do Olhar”, é uma formação transformadora que vai muito além de ensinar técnicas.</p>
+
+Este curso foi criado para quem se quer tornar profissional na área e quem quer elevar-se e destacar- se na área, dominando as tendências mais avançadas e oferecendo serviços de excelência que encantam e fidelizam clientes.
+
+Mais do que uma simples aprimoração, este curso é a tua oportunidade de
+te tornares uma profissional certificada na área completa do olhar. Aqui, vais dominar todas as técnicas essenciais, desde o design de sobrancelhas, threading, pintura de henna e brow lamination, até ao lifting e extensões de pestanas, culminando com maquilhagem específica para olhos e sobrancelhas.
+
+Cada módulo foi cuidadosamente estruturado para te capacitar com as ferramentas necessárias para oferecer um serviço personalizado e de alto impacto. Vais aprender a criar resultados sofisticados, que destacam a beleza única de cada cliente, e a garantir que cada experiência seja memorável.
+
+O curso também abrange conceitos fundamentais, como higiene, segurança e uso da ficha de anamnese, preparando-te para atuar com o máximo profissionalismo e credibilidade. Este é o curso mais completo para quem deseja ser uma referência no mercado, dominando todas as técnicas que compõem o universo do olhar.
+
+Estás pronta para te transformar numa especialista capaz de realçar não apenas a beleza exterior, mas também a confiança das tuas clientes?
+
+  O poder do olhar está agora ao teu alcance!
+
+Junta-te a esta formação e dá o próximo passo para construir uma carreira de sucesso!
+
+</h3>
+        </div>
+    </div>
+
+</section>
+
+
+
+<section class="section-three">
+    <div class="content-three">
+        <div class="titulo">CONTEÚDO PROGRAMÁTICO</div>
+            <div class="esquerda">
+            <div class="cont"><i class="fa fa-user"></i> <p class="texto">Brow Lamination<b> (4h)</b></p> </div>
+            <div class="cont"><i class="fa fa-user"></i> <p class="texto">Brow Lamination<b> (4h)</b></p> </div>
+            <div class="cont"><i class="fa fa-user"></i> <p class="texto">Brow Lamination<b> (4h)</b></p> </div>
+            <div class="cont"><i class="fa fa-user"></i> <p class="texto">Brow Lamination<b> (4h)</b></p> </div>
             </div>
-        </div>
-        <div class="scroll-indicators">
-            <span class="indicator active"></span>
-            <span class="indicator"></span>
-            <span class="indicator"></span>
-        </div>
-    </section>
-
-
-    <!-- 
-    <section class="testimonials" id="estetica">
-        <div class="testimonials-carousel">
-            <div class="testimonial-card">
-                <img class="testimonial-image" src="" alt="">
-                <div class="testimonial-right">
-                    <h3>Beauty Care</h3>
-                    <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
-                    <p class="author">Teresa Sobral</p>
-                    <p class="role">/ Advogada</p>
-                </div>
+            <div class="direita">
+            <div class="cont"><i class="fa fa-user"></i> <p class="texto">Brow Lamination<b> (4h)</b></p> </div>
+            <div class="cont"><i class="fa fa-user"></i> <p class="texto">Brow Lamination<b> (4h)</b></p> </div>
+            <div class="cont"><i class="fa fa-user"></i> <p class="texto">Brow Lamination<b> (4h)</b></p> </div>
+            <div class="cont"><i class="fa fa-user"></i> <p class="texto">Brow Lamination<b> (4h)</b></p> </div>
+            <button class="cont-btn">Quero me inscrever</button>
             </div>
-            <div class="testimonial-card">
-                <img class="testimonial-image" src="" alt="">
-                <div class="testimonial-right">
-                    <h3>Beauty Care</h3>
-                    <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
-                    <p class="author">Amanda Batista</p>
-                    <p class="role">/ Engenheira</p>
-                </div>
-
-            </div>
-            <div class="testimonial-card">
-                <img class="testimonial-image" src="" alt="">
-                <div class="testimonial-right">
-                    <h3>Beauty Care</h3>
-                    <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
-                    <p class="author">Carla Lopez</p>
-                    <p class="role">/ Professora</p>
-                </div>
-
-            </div>
+    </div>
+</section>
+<section class="four">
+    <div class="content">
+        <div class="cont">
+            
         </div>
-        <div class="scroll-indicators">
-            <span class="indicator active"></span>
-            <span class="indicator"></span>
-            <span class="indicator"></span>
-        </div>
-    </section>-->
+    </div>
+</section>
 
-    <footer id="contato">
-        <div class="footer-content">
-            <img src="assets/Logo__13 cópia 2.png" alt="">
-            <div class="informations">
-                <div>
-                    <p>Endereço: 133/2 - Lisboa - PT</p>
-                    <p>www.bonadea.pt</p>
-                </div>
-                <div>
-                    <p>contato@bonadea.pt</p>
-                    <p>91264-5395</p>
-                </div>
-                <div>
-                    <p>De Terça a Sábado das 9h às 19h</p>
-                    <p>Mais de 160 MIL Mulheres transformadas</p>
-                </div>
-            </div>
-            <p>www.bonadea.pt. All Right Reserved</p>
-        </div>
-    </footer>
-
-    <script>
-        // Progress bar
-        window.addEventListener('scroll', () => {
-            const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-            const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-            const scrolled = (winScroll / height) * 100;
-            document.querySelector('.progress-bar').style.width = scrolled + '%';
-        });
-    </script>
-</body>
-
-</html>
+<?php
+get_footer();
+?>
